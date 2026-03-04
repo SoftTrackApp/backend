6f8097edbb3f5d7fe20c387257fcc87f92a0bb3f
Kirill Siukhin <kirillsiukhin@proton.me>
2026-03-04 16:10:30 +0500

    docs: move docs to /docs, delete other files

--- softtrack/src/main/java/ru/softtrack/common/ConnectionFactory.java
+++
@@ -1,8 +0,0 @@
-package ru.softtrack.common;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionFactory {
-    Connection getNewConnection() throws SQLException;
-}
